d53744d02b13f94b0d0f6ea901bf5a40cc8621b5
AaronAcc117 <143658853+AaronAcc117@users.noreply.github.com>
2023-10-19 18:30:15 -0400

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,21 @@
+
+weight = input("Enter  weight in kilograms: ")
+height = input("Enter height in meters: ")
+
+
+    
+    weight = float(weight)
+    height = float(height)
+
+    # Calculation 
+    bmi = weight / (height * height)
+
+    
+    bmi_str = str(bmi)
+
+    
+    print("Your BMI is: " + bmi_str)
+
+
+
+
